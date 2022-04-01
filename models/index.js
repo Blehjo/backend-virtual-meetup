8ab3a456f1c6e768e672818bd0073866a978c774
bseton11-YJKnqQ <bseton11@gmail.com>
2022-04-01 04:43:41 -0500

    fixed required model in index.js

--- models/index.js
+++ models/index.js
@@ -3,6 +3,6 @@ const Profile = require('./Profile');
 const Group = require('./Group');
 const Event = require('./Event');
 const Post = require('./Post');
-const Comment = require('.Comment');
+const Comment = require('./Comment');
 
 module.exports = { User, Profile, Group, Event, Post, Comment };
