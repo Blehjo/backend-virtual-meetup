de1a6aa4f92b73024643035197f1416392fb4392
bseton11-YJKnqQ <bseton11@gmail.com>
2023-05-02 20:03:13 -0500

    adding boiler plate code

--- models/index.js
+++
@@ -1,8 +0,0 @@
-const User = require('./User');
-const Profile = require('./Profile');
-const Group = require('./Group');
-const Event = require('./Event');
-const Post = require('./Post');
-const Comment = require('./Comment');
-
-module.exports = { User, Profile, Group, Event, Post, Comment };
